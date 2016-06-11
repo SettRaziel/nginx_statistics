82e49c93a5882f94b7260f69bf2c340bbe2ff509
Benjamin Held <benjamin.held@stud.hs-hannover.de>
2016-06-11 16:40:32 +0200

    added documentation script - added shellscript to create yard documentation - adjusted gitignore to prevent pushing yard documentation files

---
+++ create_yard.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# @Author: Benjamin Held
+# @Date:   2015-08-30 09:48:37
+# @Last Modified by:   Benjamin Held
+# @Last Modified time: 2016-05-28 09:54:05
+
+exec yard doc --private --readme ./README.md
