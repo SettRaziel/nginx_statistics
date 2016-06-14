92f78f50cf147b271c5a8504393c7acd3e2eb505
Benjamin Held <benjamin.held@stud.hs-hannover.de>
2016-06-14 20:07:55 +0200

    extended statistic module - added method to create a ranking over the constructed index

--- lib/statistic/statistic.rb
+++ lib/statistic/statistic.rb
@@ -1,7 +1,7 @@
 # @Author: Benjamin Held
 # @Date:   2016-04-19 21:31:13
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2016-06-06 15:16:33
+# @Last Modified time: 2016-06-14 19:52:27
 
 # module to generate the required statisitc result for the requested attribute
 module Statistic
@@ -27,6 +27,21 @@ module Statistic
 
     ranking = mapping.sort_by {|k, v| [v, k] }.reverse
     return ranking.first(rank)
+  end
+
+  # method to generate a top n ranking over the calculated index
+  # @param [Hash] index the calculated index of the entries
+  # @param [Integer] number_of_results the requested number of results
+  # @return [Hash] the sorted n result with the highest ranking mapped as
+  #   {result => occurrence}
+  def self.generate_ranking_for_index(index, number_of_results)
+    mapping = Hash.new()
+    index.each_pair { |key, value|
+      mapping[key] = value.length
+    }
+
+    ranking = mapping.sort_by {|k, v| [v, k] }.reverse
+    return ranking.first(number_of_results)
   end
 
 end
