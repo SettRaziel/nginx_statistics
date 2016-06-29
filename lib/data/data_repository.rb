90cbf46451f0fc3c076269a63cac255f7473be80
Benjamin Held <benjamin.held@stud.hs-hannover.de>
2016-06-29 15:34:48 +0200

    reworked data_repository - corrected and adjusted documentation - removed and adjusted code

--- lib/data/data_repository.rb
+++ lib/data/data_repository.rb
@@ -1,45 +1,51 @@
 # @Author: Benjamin Held
 # @Date:   2016-04-08 17:05:43
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2016-05-26 11:42:37
+# @Last Modified time: 2016-06-29 15:32:26
 
 require_relative 'file_reader'
 require_relative 'entry'
+require_relative '../output/string'
 
 # This class serves as a data repository storing the read data and handling the
 # meta information
 class DataRepository
-  # @return Hash mapping {Key} -> {Array}
+  # @return Hash returns specific mapping on the given key (values => Array)
   attr_reader :index
   # @return Array an array containing all entries
   attr_reader :repository
 
   # initialization
-  # @param [String] filename filepath
+  # @param [String] filename the filepath
+  # @param [Symbol] key the attribute for the index
   def initialize(filename, key = nil)
     @index = Hash.new()
     @repository = Array.new()
     begin
       create_entries(read_file(filename))
-      create_mapping(key) if (key != nil)
+      if (key != nil)
+        create_mapping(key)
+      else
+        create_mapping(:http_status)
+      end
     rescue Exception => e
-      puts "Error: #{e.message}"
+      puts "Error: #{e.message}".red
     end
   end
 
   # method to get the number of entries for the given key
-  # @param [String] the requested key
+  # @param [String] key the requested key
   # @return [Integer] the number of entries
   def get_count_to_key(key)
     if (!@index.empty?)
       @index[key].size
     else
-      puts 'Error: index is empty. No index has been created!'
+      puts 'Error: index is empty. No index has been created!'.red
     end
   end
 
-  # method to retrieve the number of entries for all keys
-  # @return [Hash] a mapping of key => number of entries
+  # method to retrieve the number of entries for every key
+  # @return [Hash] a mapping of (key => number of entries)
   def get_keys_and_their_count
     result = Hash.new()
     @index.each_key { |key|
@@ -50,26 +56,16 @@ class DataRepository
 
   private
 
-  # reads the file and creates data of its content with default meta
-  # information
-  # @param [String] filename filepath
-  # @return [MetaData] the meta data object for this data
-  def add_data_with_default_key(data)
-    create_entries(data, :timestamp)
-  end
-
-  # creates {DataSet}s of the parsed data and stores it into a {DataSeries}
+  # creates {Entry}s of the parsed data and stores it in the repository
   # @param [Array] data the read data
-  # @return [DataSeries] the data series created by the data input
   def create_entries(data)
     data.each { |line|
       @repository << Entry::Entry.new(line) if (!line.empty?)
     }
   end
 
-  # creates {DataSet}s of the parsed data and stores it into a {DataSeries}
-  # @param [Array] data the read data
-  # @return [DataSeries] the data series created by the data input
+  # creates an index, mapping all {Entry}s
+  # @param [Symbol] key the index attribute
   def create_mapping(key)
     @repository.each { |entry|
         mapped_key = map_key_to_attribute(key, entry)
