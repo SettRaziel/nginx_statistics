c7fce4a6b9fc4927968832c4af975636f773633b
Benjamin Held <benjamin.held@stud.hs-hannover.de>
2016-07-24 18:29:02 +0200

    adjusted listenable module

--- lib/event/listenable.rb
+++ lib/event/listenable.rb
@@ -1,28 +1,39 @@
 # @Author: Benjamin Held
 # @Date:   2016-07-14 18:05:20
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2016-07-22 08:51:57
+# @Last Modified time: 2016-07-24 18:28:21
 
+# module to implement simple event handling and notifying
 module Listenable
 
-    def listeners
-      @listeners ||= Hash.new()
-    end
+  # method to get get alle listener or create a new Hash, when not listener
+  # is present
+  def listeners
+    @listeners ||= Hash.new()
+  end
 
-    def add_listener(identification, listener)
-      listeners[identification] = listener
-    end
+  # method to add a listener which wants to be notified when a given event
+  # is triggered
+  # @param [Symbol] identification an identification for the listener
+  # @param [Object] listener the listener object that should be added as a
+  #    new listener
+  def add_listener(identification, listener)
+    listeners[identification] = listener
+  end
 
-    def remove_listener(listener)
-      listeners.delete(listener)
-    end
+  # method to remove a given listener from the list of listener objects
+  def remove_listener(listener)
+    listeners.delete(listener)
+  end
 
-    def notify_listeners(event_name, *args)
-      listeners.each { |listener|
-          if listener.respond_to?(event_name)
-            listener.__send__(event_name, *args)
-          end
-      }
-    end
+  # method to send a notification to all listeners which respond to the
+  # given event
+  def notify_listeners(event_name, *args)
+    listeners.each { |listener|
+      if listener.respond_to?(event_name)
+        listener.__send__(event_name, *args)
+      end
+    }
+  end
 
 end
