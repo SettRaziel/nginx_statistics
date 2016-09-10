df951eb1115741808ffb0310617420f0d017ef15
Benjamin Held <benjamin.held@stud.hs-hannover.de>
2016-09-10 16:42:24 +0200

    added additional listener methods

--- lib/event/repository_listener.rb
+++ lib/event/repository_listener.rb
@@ -1,10 +1,11 @@
 # @Author: Benjamin Held
 # @Date:   2016-07-15 15:43:54
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2016-08-21 22:35:06
+# @Last Modified time: 2016-09-10 16:41:28
 
 require_relative '../output/string'
 require_relative '../statistic/statistic.rb'
+require_relative '../menu/menu'
 
 # helper class which create a connection between event handling and the
 # {DataRepository}
@@ -34,6 +35,29 @@ class RepositoryListener
   # method to generate the ranking over the index and print its results
   def generate_and_print_index
     ranking = Statistic.generate_ranking_for_index(@data_repository.index, 10)
+    puts 'Output as: number of occurence | entry content'.yellow
+    ranking.each { |entry|
+      print "%5s ".red % [entry[1]]
+      puts "times: #{(entry[0]).to_s.magenta} "
+    }
+  end
+
+  # method to generate the required ranking and present a menu to select
+  # the key for the subselect on that index
+  def initialize_subselect
+    ranking = Statistic.generate_ranking_for_index(@data_repository.index, 10)
+    subselect_menu = Menu::SubselectMenu.new(ranking)
+    subselect_menu.add_listener(:repo_listener, self)
+    subselect_menu.print_menu
+  end
+
+  # method to retrieve all entries with the value at the given criteria from
+  # the index and print the top n ranking of the result
+  # @param [Object] value the search value
+  # @param [Symbol] criteria the ranking criteria
+  def generate_and_print_subselect(value, criteria)
+    entries = @data_repository.get_entries_from_index_to(value)
+    ranking = Statistic.generate_ranking_for(entries, 10, criteria)
     puts 'Output as: number of occurence | entry content'.yellow
     ranking.each { |entry|
       print "%5s ".red % [entry[1]]
