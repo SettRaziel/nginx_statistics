1f0d188993300770be3e52051f2f99e97b9a5ba7
Benjamin Held <benjamin.held@stud.hs-hannover.de>
2016-09-14 20:40:08 +0200

    added documentation to subselect menu

--- lib/menu/subselect_menu.rb
+++ lib/menu/subselect_menu.rb
@@ -1,19 +1,29 @@
 # @Author: Benjamin Held
 # @Date:   2016-07-13 10:12:17
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2016-09-11 18:40:37
+# @Last Modified time: 2016-09-14 20:38:17
 
+# This module groups the different menu classes that are used to for the
+# terminal options. The class {Base} provides the basic methods that are needed.
+# Child classes only need to implement the method {Base#define_menu_items} and
+# {Base#determine_action}.
 module Menu
+
 
   class SubselectMenu < Base
     include Listenable
 
+    # initialization
+    # @param [Hash] the sorted n result with the highest ranking mapped as
+    #   (attribute => occurrence)
     def initialize(ranking)
       @menu_description = 'Overview of the top n index results:'
       @ranking = ranking
       super
     end
 
+    # method to replace the current criteria
+    # @param [Symbol] criteria the ranking criteria
     def change_index(criteria)
       @criteria = criteria
     end
@@ -22,8 +32,10 @@ module Menu
 
     # @return [Array] an array that holds arrays of size two with [value, rank]
     attr :ranking
+    # @return [Symbol] criteria the ranking criteria
     attr :criteria
 
+    # implementation to define the items of the menu
     def define_menu_items
       @ranking.each { |entry|
         text = "#{entry[0]} [#{entry[1]}]"
@@ -32,6 +44,8 @@ module Menu
       add_menu_item("Return to previous menu.")
     end
 
+    # method to specify the actions to a given input
+    # @param [String] input the user input
     def determine_action(input)
       input_number = input.to_i
       case
