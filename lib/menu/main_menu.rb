085530f13a65d72f2ddc3eca28e8180deee3926e
Benjamin Held <benjamin.held@stud.hs-hannover.de>
2016-09-17 17:56:09 +0200

    added documentation to main menu

--- lib/menu/main_menu.rb
+++ lib/menu/main_menu.rb
@@ -1,21 +1,24 @@
 # @Author: Benjamin Held
 # @Date:   2016-07-14 17:40:05
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2016-09-12 19:20:57
+# @Last Modified time: 2016-09-17 17:54:37
 
 module Menu
 
   require_relative '../event/listenable'
   require_relative '../output/string'
 
+  # menu class that inherits {Menu::Base} to abstract the main menu
   class MainMenu < Base
     include Listenable
 
+    # initialization
     def initialize
       super
       @menu_description = 'Main menu. Select operation:'
     end
 
+    # implementation to define the items of the menu
     def define_menu_items
       add_menu_item('Create different index.', 1)
       add_menu_item('Print current index.', 2)
@@ -23,6 +26,8 @@ module Menu
       add_menu_item('Quit.', 4)
     end
 
+    # method to specify the actions to a given input
+    # @param [String] input the user input
     def determine_action(input)
       case (input.to_i)
         when 1
