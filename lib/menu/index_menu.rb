a40b00f9e39453e1924617cbb1ceff39f30e1bab
Benjamin Held <benjamin.held@stud.hs-hannover.de>
2016-09-18 17:36:43 +0200

    added documentation to index menu

--- lib/menu/index_menu.rb
+++ lib/menu/index_menu.rb
@@ -1,13 +1,16 @@
 # @Author: Benjamin Held
 # @Date:   2016-07-15 16:06:08
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2016-07-28 16:21:01
+# @Last Modified time: 2016-09-18 17:32:42
 
 module Menu
 
+  # menu class that inherits {Menu::Base} to abstract the menu to select a
+  # given status
   class IndexMenu < Base
     include Listenable
 
+    # initialization
     def initialize
       @menu_description = 'Index menu. Select the criteria for the index:'
       super
@@ -15,6 +18,7 @@ module Menu
 
     private
 
+    # implementation to define the items of the menu
     def define_menu_items
       add_menu_item('Http request', 1)
       add_menu_item('Http status', 2)
@@ -22,6 +26,8 @@ module Menu
       add_menu_item('Timestamp', 4)
     end
 
+    # method to specify the actions to a given input
+    # @param [String] input the user input
     def determine_action(input)
       case (input.to_i)
         when 1 then notify_listeners(:change_index, :http_request)
