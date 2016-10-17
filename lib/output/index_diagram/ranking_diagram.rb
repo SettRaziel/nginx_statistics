feb61a15579a6f0412e52310c859439534388c51
Benjamin Held <benjamin.held@stud.hs-hannover.de>
2016-10-17 17:04:58 +0200

    added documentation to ranking diagram class

--- lib/output/index_diagram/ranking_diagram.rb
+++ lib/output/index_diagram/ranking_diagram.rb
@@ -1,12 +1,16 @@
 # @Author: Benjamin Held
 # @Date:   2016-10-03 23:30:13
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2016-10-16 12:52:26
+# @Last Modified time: 2016-10-17 17:03:37
 
 require_relative 'terminal_size'
 
+# logic class to draw a simple bar chart for the given ranking
 class RankingDiagram
 
+  # initialization
+  # @param [Array] ranking an array storing the values and the numbers of
+  #   occurences in nested arrays
   def initialize(ranking)
     @entry_size = 20
     retrieve_terminal_width
@@ -20,15 +24,21 @@ class RankingDiagram
   end
 
   private
+  # @return [Number] the width available for the bars
   attr_accessor :width
+  # @return [Number] the overall number of considered entries
   attr_accessor :count
+  # @return [Number] the maximal length of the value characters that are printed
   attr_accessor :entry_size
 
+  # method to get the size information of the used terminal
   def retrieve_terminal_width
     ts = TerminalSize::TerminalSize.new()
     @width = ts.columns - @entry_size - 3
   end
 
+  # method to print the value, the bar and the percentage of the occurence based
+  # on the overall number of considered entries
   def print_hash_pair(entry)
     print "%#{@entry_size}s |" % [entry[0].to_s[0...@entry_size]]
     ratio = entry[1] * 1.0 / @count
@@ -40,6 +50,7 @@ class RankingDiagram
     puts " (#{(ratio * 100).round(2)}%)"
   end
 
+  # method to print the x data axis
   def print_x_axis
     print "%#{@entry_size}s |" % ["portion in %"]
 
@@ -59,6 +70,7 @@ class RankingDiagram
     puts "\n#{axis_numbers}"
   end
 
+  # helper method to calculate the number of all considered entries
   def accumulate_numbers(ranking)
     @count = 0
     ranking.each { |entry|
@@ -66,6 +78,9 @@ class RankingDiagram
     }
   end
 
+  # helper method to initialize the string printing the number values of
+  # the x axis
+  # @return [String] the starting characters of the x axis number string
   def initialize_axis_numbers
     axis = ""
     (entry_size + 1).times { axis.concat(" ")}
