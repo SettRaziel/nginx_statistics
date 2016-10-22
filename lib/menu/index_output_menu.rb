0a1f9a5c17e83b6fc5c2d0387e505d2a80fa8fcb
Benjamin Held <benjamin.held@stud.hs-hannover.de>
2016-10-22 14:33:22 +0200

    replaced false menu file

--- lib/menu/index_output_menu.rb
+++ lib/menu/index_output_menu.rb
@@ -1,28 +1,28 @@
 # @Author: Benjamin Held
-# @Date:   2016-10-19 23:30:19
+# @Date:   2016-10-21 11:39:23
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2016-10-19 23:32:41
+# @Last Modified time: 2016-10-21 12:14:29
 
 module Menu
 
   require_relative '../event/listenable'
   require_relative '../output/string'
 
-  # menu class that inherits {Menu::Base} to create a menu for the provided
-  # index creation options
-  class DiagramMenu < Base
+  # menu class that inherits {Menu::Base} to create a menu for creating output
+  # of a selected index
+  class IndexOutputMenu < Base
     include Listenable
 
     # initialization
     def initialize
       super
-      @menu_description = 'Bar diagram menu. Select operation:'
+      @menu_description = 'Index operation menu. Select operation:'
     end
 
     # implementation to define the items of the menu
     def define_menu_items
-      add_menu_item('Bar diagram for main index.', 1)
-      add_menu_item('Bar diagram for subselect index.', 2)
+      add_menu_item('Output main index.', 1)
+      add_menu_item('Output subselect index.', 2)
       add_menu_item('Return to previous menu.', 3)
     end
 
@@ -31,9 +31,9 @@ module Menu
     def determine_action(input)
       case (input.to_i)
         when 1
-          # print normal index diagram
+          notify_listeners(:generate_and_print_index)
         when 2
-          # check for subselect; print subselect diagram
+          notify_listeners(:output_ranking)
         when 3 then return
       end
     end
