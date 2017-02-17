67ee83e33ae569cf70b4ea82f5c68dc5172bf074
Benjamin Held <me@bheld.eu>
2017-02-17 17:29:04 +0100

    adjusted menu module - moved base menu to its own class - added imports to menu file

--- lib/menu/menu.rb
+++ lib/menu/menu.rb
@@ -1,89 +1,18 @@
 # @Author: Benjamin Held
 # @Date:   2016-07-11 14:01:19
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2016-10-29 14:20:36
+# @Last Modified time: 2017-02-17 17:27:06
 
 module Menu
 
   require_relative '../output/string'
 
-  # abstract parent class for the menu hierarchy
-  class Base
-
-    # initialization
-    # @param [String] description the headline of the menu
-    def initialize(description=nil)
-      @menu_items = Hash.new()
-      if (description == nil)
-        @menu_description = "Default menu. Please add description: "
-      end
-      define_menu_items
-      @menu_items = Hash[@menu_items.sort]
-    end
-
-    # method to print the menu to the terminal and wait for input
-    def print_menu
-      puts @menu_description
-      @menu_items.each_pair { |key, value|
-        puts "(#{key}) #{value}"
-      }
-
-      determine_action(get_entry('Select option: '))
-    end
-
-    private
-
-    # @return [Hash] a hash which maps (number => string) for the menu items
-    attr :menu_items
-    # @return [String] a string with the head description of the menu
-    attr :menu_description
-
-    # @abstract subclasses need to implement this method
-    # @raise [NotImplementedError] if the subclass does not have this method
-    def define_menu_items
-      fail NotImplementedError, " Error: the subclass " \
-        "#{self.name.split('::').last} needs to implement the method: " \
-        "define_menu_items from its base class".red
-    end
-
-    # @abstract subclasses need to implement this method
-    # @param [String] input the provided user input
-    # @raise [NotImplementedError] if the subclass does not have this method
-    def determine_action(input)
-      fail NotImplementedError, " Error: the subclass " \
-        "#{self.name.split('::').last} needs to implement the method: " \
-        "determine_action from its base class".red
-    end
-
-    # default behavior when a user provides not valid input
-    def handle_wrong_option
-      print 'Option not available. '.red
-      determine_action(get_entry('Select option: '))
-    end
-
-    # method to add a given key/value pair to the menu hash
-    # @param [String] description the description of the menu item
-    # @param [Integer] index the index that should be used as key
-    def add_menu_item(description, index=nil)
-      index = @menu_items.length + 1 if (index == nil)
-      @menu_items[index] = description
-    end
-
-    # method to print a given message and read the provided input
-    # @param [String] message output message
-    # @return [String] the input from the terminal
-    def get_entry(message)
-      print message.blue.bright
-      gets.chomp
-    end
-
-  end
+  require_relative 'base_menu'
+  require_relative 'diagram_menu'
+  require_relative 'main_menu'
+  require_relative 'index_output_menu'
+  require_relative 'index_overview_menu'
+  require_relative 'index_selection_menu'
+  require_relative 'subselect_menu'
 
 end
-
-require_relative 'diagram_menu'
-require_relative 'main_menu'
-require_relative 'index_output_menu'
-require_relative 'index_overview_menu'
-require_relative 'index_selection_menu'
-require_relative 'subselect_menu'
